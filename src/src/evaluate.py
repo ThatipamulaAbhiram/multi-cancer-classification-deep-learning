31fbedbbf9ae0490fe6c5eda74d48d976a298ed2
Thatipamula Abhiram <abhiramthatipamula16@gmail.com>
2026-02-13 18:03:47 +0530

    Create evaluate.py

---
+++ src/src/evaluate.py
@@ -0,0 +1,10 @@
+"""
+Evaluation Module - Multi Cancer Classification
+Author: Abhiram Thattapamala
+"""
+
+def evaluate_model():
+    print("Evaluating trained model performance...")
+
+if __name__ == "__main__":
+    evaluate_model()
