6cc67da87b9551ed97a37d714c26f189bfcf116c
Thatipamula Abhiram <abhiramthatipamula16@gmail.com>
2026-02-13 17:50:09 +0530

    Create train.py

---
+++ src/train.py
@@ -0,0 +1,10 @@
+"""
+Training Pipeline - Multi Cancer Classification
+Author: Abhiram Thattapamala
+"""
+
+def train_model():
+    print("Starting training pipeline for multi-cancer classification...")
+
+if __name__ == "__main__":
+    train_model()
