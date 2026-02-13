854ede53624ded4bd1c07c620a3ce05358747c4d
Thatipamula Abhiram <abhiramthatipamula16@gmail.com>
2026-02-13 17:51:02 +0530

    Create preprocess.py

---
+++ src/src/preprocess.py
@@ -0,0 +1,9 @@
+"""
+Preprocessing Module - Multi Cancer Classification
+"""
+
+def preprocess_data():
+    print("Running image preprocessing and augmentation...")
+
+if __name__ == "__main__":
+    preprocess_data()
