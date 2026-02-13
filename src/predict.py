9f306e6ad57db6ec4f80394497d77b7171f8a432
Thatipamula Abhiram <abhiramthatipamula16@gmail.com>
2026-02-13 18:08:11 +0530

    Create predict.py

---
+++ src/predict.py
@@ -0,0 +1,10 @@
+"""
+Prediction Module - Multi Cancer Classification
+Author: Abhiram Thattapamala
+"""
+
+def predict():
+    print("Running cancer classification prediction...")
+
+if __name__ == "__main__":
+    predict()
